f83d2bd9329a137bb77354f4d88af2998d3a16e4
Shashwat-Kush <shashwatkush002@kgpian.iitkgp.ac.in>
2025-12-23 02:52:42 +0530

    Data transformation Implemented

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from textSummarizer.logging import logger
 
 from textSummarizer.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
+from textSummarizer.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+
 STAGE_NAME = 'Data Ingestion'
 
 try:
@@ -20,6 +22,18 @@ try:
     logger.info(f">>>>> Stage {STAGE_NAME} started <<<<<")
     data_validation = DataValidationTrainingPipeline()
     data_validation.main()
+    logger.info(f">>>>> Stage {STAGE_NAME} completed <<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise(e)
+
+
+STAGE_NAME = 'Data Transformation'
+
+try:
+    logger.info(f">>>>> Stage {STAGE_NAME} started <<<<<")
+    data_tranformation = DataTransformationTrainingPipeline()
+    data_tranformation.main()
     logger.info(f">>>>> Stage {STAGE_NAME} completed <<<<<")
 except Exception as e:
     logger.exception(e)
